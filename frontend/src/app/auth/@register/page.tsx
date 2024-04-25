eba410e17ff6dbaa6a0e3f803d5a2b5e112346f6
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-04-25 03:06:36 +0300

    added useStates

--- frontend/src/app/auth/@register/page.tsx
+++ frontend/src/app/auth/@register/page.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "./page.css";
 import mainLogo from "../../../../public/images/mainLogo.png";
 
-const Register = () => {
+const Register: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [step, setStep] = useState(1);
+
   return (
     <div className="register-container">
       <div className="register-logo">
@@ -12,6 +18,7 @@ const Register = () => {
         <h1>Join BookHub</h1>
         <h1>Where Stories Unite!</h1>
         <input placeholder="Email"></input>
+        <button>Continue</button>
       </div>
     </div>
   );
