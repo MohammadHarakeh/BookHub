9db09037e70f441bb9d6b99a5a9ea914d9a66383
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-04-30 00:02:20 +0300

    added the email.js controller

---
+++ backend/controller/email.js
@@ -0,0 +1,29 @@
+const crypto = require("crypto");
+const User = require("../models/User");
+const { sendPasswordResetEmail } = require("../utils/email");
+
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const token = crypto.randomBytes(20).toString("hex");
+
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000;
+    await user.save();
+
+    await sendPasswordResetEmail(email, token);
+
+    return res.status(200).json({ message: "Password reset email sent" });
+  } catch (error) {
+    console.error("Error in forgotPassword:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { forgotPassword };
