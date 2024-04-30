2e0c9a51adc2e3a48f06517ddd195d58593543d0
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-04-30 20:44:07 +0300

    added forgotPassword function in the frontend

--- frontend/src/app/(auth)/forgotPassword/page.tsx
+++ frontend/src/app/(auth)/forgotPassword/page.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./../shared.css";
 import styles from "./page.module.css";
 import mainLogo from "../../../../public/images/mainLogo.png";
 import Link from "next/link";
+import { sendRequest } from "../../tools/apiRequest";
+import { requestMethods } from "../../tools/apiRequestMethods";
+import { useRouter } from "next/navigation";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState<string>("");
+
+  const router = useRouter();
+
+  const forgotPassword = async () => {
+    try {
+      const body = JSON.stringify({
+        email: email,
+      });
+
+      const response = await sendRequest(
+        requestMethods.POST,
+        `/user/forgotPassword`,
+        body
+      );
+
+      if (response.status === 200) {
+        setEmail("");
+        router.push("/changePassword");
+        toast.success("Email has been sent");
+      } else {
+        toast.error("Failed to send email");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Error sending email");
+    }
+  };
+
   return (
     <div className="register-container">
       <ToastContainer
@@ -23,14 +57,22 @@ const ForgotPassword = () => {
         </div>
 
         <div className={styles.forgotpassword_input}>
-          <input className="general-input" placeholder="Email"></input>
+          <input
+            className="general-input"
+            placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          ></input>
         </div>
 
         <div className={styles.forgotpassword_switcher}>
           <Link href="/login" className="general-button">
             Back
           </Link>
-          <button className="general-button">Contiue</button>
+          <button className="general-button" onClick={forgotPassword}>
+            Contiue
+          </button>
         </div>
       </div>
     </div>
