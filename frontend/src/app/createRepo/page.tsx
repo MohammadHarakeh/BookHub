a6abe9b5ee8444ab4e263cdf3e835a4182c9e6e9
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-05 20:03:41 +0300

    frontend <> added public and private in create repo

--- frontend/src/app/createRepo/page.tsx
+++ frontend/src/app/createRepo/page.tsx
@@ -3,6 +3,7 @@ import "./page.css";
 import "../globals.css";
 import Header from "../component/header/page";
 import Footer from "../component/footer/page";
+import { FaBook, FaLock } from "react-icons/fa";
 
 const CreateRepo = () => {
   return (
@@ -12,18 +13,53 @@ const CreateRepo = () => {
         <div className="story-title">
           <p>Create a new story</p>
         </div>
+        <hr />
 
         <div className="name-description-wrapper">
           <div className="story-owner">
             <p>Owner</p>
             <input type="text" className="general-input"></input>
           </div>
-          <p className="test">/</p>
+          <p className="story-seperator">/</p>
           <div className="story-name">
             <p>Story name</p>
             <input type="text" className="general-input"></input>
           </div>
         </div>
+        <div className="story-description">
+          <p>Description</p>
+          <input
+            type="text"
+            className="general-input description-input"
+          ></input>
+        </div>
+        <hr />
+
+        <div className="story-visablility">
+          <div className="story-public">
+            <input type="radio"></input>
+            <FaBook />
+            <div className="test">
+              <label>Public</label>
+              <p className="public-info">
+                Anyone on the internet can see this story. You choose who can
+                commit.
+              </p>
+            </div>
+          </div>
+
+          <div className="story-private">
+            <input type="radio"></input>
+            <FaLock />
+            <div>
+              <label>Private</label>
+              <p className="private-info">
+                You choose who can see and commit to this story.
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr />
       </div>
       <Footer />
     </div>
