44d62d6948a2fc0329a7ec63b4d9fbf314c7069a
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-08 15:12:15 +0300

    frontend <> fixed the folder structure and adde a function to get invitingUserInfo

--- frontend/src/app/invitedPage/[invitationToken]/page.tsx
+++ frontend/src/app/invitedPage/[invitationToken]/page.tsx
@@ -1,31 +1,34 @@
-import React from "react";
-import { Metadata } from "next";
+"use client";
+import React, { useState } from "react";
 import "./page.css";
-
-export const generateMetadata = async ({
-  params,
-}: Props): Promise<Metadata> => {
-  const title = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`Invitation ${params.invitationToken}`);
-    }, 100);
-  });
-
-  return {
-    title: `Invitation ${title}`,
-  };
-};
-
-type Props = {
-  params: {
-    invitationToken: string;
-  };
-};
+import Header from "@/app/component/header/page";
+import Footer from "@/app/component/footer/page";
+import { requestMethods } from "../../tools/apiRequestMethods";
+import { sendRequest } from "../../tools/apiRequest";
 
 const InvitedPage = ({ params }: { params: { invitationToken: string } }) => {
+  const [invitingUser, setInvitingUser] = useState();
+
+  const getInvitingUserInfo = async (invitingUserId: string) => {
+    try {
+      const response = sendRequest(
+        requestMethods.GET,
+        `/user/invitingUserProfile/${invitingUserId}`
+      );
+    } catch (error) {
+      console.error("Error getting user info", error);
+    }
+  };
+
   return (
-    <div className="invitation-wrapper">
-      <h1>Invited Page for token {params.invitationToken}</h1>
+    <div className="invited-wrapper">
+      <Header />
+
+      <div className="invite-card">
+        <h1>Invited Page for token {params.invitationToken}</h1>
+      </div>
+
+      <Footer />
     </div>
   );
 };
