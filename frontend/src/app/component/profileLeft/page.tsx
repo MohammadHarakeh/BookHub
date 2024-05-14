a26a5a1491ba1a50065b7653b6dc24ce924d8376
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-14 23:08:45 +0300

    frontend <> changed tab title and name

--- frontend/src/app/component/profileLeft/page.tsx
+++ frontend/src/app/component/profileLeft/page.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./page.css";
 import defaultImage from "../../../../public/images/defaultImage.png";
 import { useEmailContext } from "@/context/emailContext";
 
 const ProfileLeft = () => {
-  const { userLoggedIn, setUserLoggedIn } = useEmailContext();
+  const { userInfo } = useEmailContext();
 
   return (
     <div className="pageleft-wrapper">
       <div className="pageleft-image">
-        <img src={defaultImage.src}></img>
+        <img
+          src={userInfo.user?.profile?.profile_picture}
+          alt="User Picture"
+        ></img>
       </div>
     </div>
   );
