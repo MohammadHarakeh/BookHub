2a513a6be986c881de367b31aa6c1b23e37f7148
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-15 22:17:23 +0300

    frontend <> added new button with styling and routing to the create page

--- frontend/src/app/component/profileMiddle/page.tsx
+++ frontend/src/app/component/profileMiddle/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./page.css";
 import { FaPlus } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 const ProfileMiddle = () => {
+  const router = useRouter();
   return (
     <div className="profile-middle-wrapper">
       <div className="profile-middle-title">
@@ -11,7 +13,10 @@ const ProfileMiddle = () => {
       <div className="profile-middle-stories">
         <p>You haven't worked on any story</p>
         <p>Create a story now</p>
-        <button className="general-button">
+        <button
+          className="general-button"
+          onClick={() => router.push("/createRepo")}
+        >
           <FaPlus /> New
         </button>
       </div>
