21c1cb6ab0aa3eb98140c3a97f5c4282ac59d0f0
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-15 22:11:01 +0300

    frontend <> added profile middle stories empty state

--- frontend/src/app/component/profileMiddle/page.tsx
+++ frontend/src/app/component/profileMiddle/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./page.css";
+import { FaPlus } from "react-icons/fa";
 
 const ProfileMiddle = () => {
   return (
     <div className="profile-middle-wrapper">
       <div className="profile-middle-title">
         <p>Recently worked on stories</p>
+      </div>
+      <div className="profile-middle-stories">
+        <p>You haven't worked on any story</p>
+        <p>Create a story now</p>
+        <button className="general-button">
+          <FaPlus /> New
+        </button>
       </div>
     </div>
   );
