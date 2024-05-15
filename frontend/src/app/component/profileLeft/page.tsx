c439c298d591a009b4933113de3b64f572a8a37a
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-15 05:35:04 +0300

    frontend <> fixed location of the user info

--- frontend/src/app/component/profileLeft/page.tsx
+++ frontend/src/app/component/profileLeft/page.tsx
@@ -22,18 +22,6 @@ const ProfileLeft = () => {
       <div className="profileleft-info">
         <p>{userInfo.user?.username}</p>
         {userInfo.user?.profile?.bio && <p>{userInfo.user.profile.bio}</p>}
-        {userInfo.user?.profile?.location && (
-          <p>{userInfo.user.profile.location}</p>
-        )}
-        {userInfo.user?.profile?.linkedin_link && (
-          <p>{userInfo.user.profile.linkedin_link}</p>
-        )}
-        {userInfo.user?.profile?.instagram_link && (
-          <p>{userInfo.user.profile.instagram_link}</p>
-        )}
-        {userInfo.user?.profile?.twitter_link && (
-          <p>{userInfo.user.profile.twitter_link}</p>
-        )}
       </div>
       <div className="general-button">Edit Profile</div>
       <div className="following-info-wrapper">
@@ -45,6 +33,20 @@ const ProfileLeft = () => {
           <p>following - </p>
           {userInfo.user?.followers ? userInfo.user?.followers?.length : 0}
           <p>followers</p>
+        </div>
+        <div>
+          {userInfo.user?.profile?.location && (
+            <p>{userInfo.user.profile.location}</p>
+          )}
+          {userInfo.user?.profile?.linkedin_link && (
+            <p>{userInfo.user.profile.linkedin_link}</p>
+          )}
+          {userInfo.user?.profile?.instagram_link && (
+            <p>{userInfo.user.profile.instagram_link}</p>
+          )}
+          {userInfo.user?.profile?.twitter_link && (
+            <p>{userInfo.user.profile.twitter_link}</p>
+          )}
         </div>
       </div>
     </div>
