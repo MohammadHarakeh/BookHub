b61392252c069da7e4b85c0febfee1e1a5b38adf
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-16 20:25:46 +0300

    frontend <> added repository image to the versions

--- frontend/src/app/storyVersions/page.tsx
+++ frontend/src/app/storyVersions/page.tsx
@@ -4,13 +4,20 @@ import "./page.css";
 import Header from "../component/header/page";
 import Footer from "../component/footer/page";
 import { useEmailContext } from "@/context/emailContext";
+import defaultImage from "../../../public/images/defaultImage.png";
 
 const StoryVersions = () => {
   const { storyVersions, setStoryVersions } = useEmailContext();
   return (
     <div className="story-version-wrapper">
       <Header />
-      <div className="story-version-container">{storyVersions.name}</div>
+      <div className="story-version-container">
+        {storyVersions ? (
+          <img src={storyVersions.repo_picture} />
+        ) : (
+          <img src={defaultImage.src} />
+        )}
+      </div>
       <Footer />
     </div>
   );
