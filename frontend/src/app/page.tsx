815667e3e5673e7780de1e2ce7742d4729457449
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-05-18 01:32:49 +0300

    frontend <> added lightmode for the main-page wrapper

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,13 +6,16 @@ import Footer from "../app/component/footer/page";
 import HomeLeft from "../app/component/homeLeft/page";
 import HomeMiddle from "../app/component/homeMiddle/page";
 import HomeRight from "../app/component/homeRight/page";
+import { useEmailContext } from "@/context/emailContext";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+  const { themeMode } = useEmailContext();
+
   return (
-    <div className="home-wrapper">
+    <div className={`home-wrapper ${themeMode === "dark" ? "" : "light-mode"}`}>
       <ToastContainer
         theme="dark"
         toastStyle={{ backgroundColor: "#0e0f32" }}
